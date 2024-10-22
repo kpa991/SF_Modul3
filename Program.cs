8bbecf630c6c051dc5d0fbab50c2aadaf30b3005
kpa991 <pklimov41@gmail.com>
2024-10-22 12:59:46 +0300

    Выполнил задание 3.2.8

--- Program.cs
+++ Program.cs
@@ -8,6 +8,11 @@
             MyName = "Pavel";
             Console.WriteLine(MyName);
             Console.WriteLine("Привет, \n мир");
+            Console.WriteLine("\t Привет Мир");
+            Console.WriteLine("\t Мне 27 лет");
+            Console.WriteLine("\t My name is \n {0}", MyName);
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\x23");
             Console.ReadKey();
 
         }
